0d86921c5c7af1d978a70da124ed26e3ea50a769
Daniil Baturin <daniil@baturin.org>
2015-03-05 17:39:05 +0600

    Set exit status to 0 if hypervisor is detected and to 1 otherwise.

--- src/hvinfo.adb
+++ src/hvinfo.adb
@@ -1,6 +1,7 @@
 with Interfaces; use Interfaces;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Command_Line; use Ada.Command_Line;
 
 with Hypervisor_Check; use Hypervisor_Check;
 procedure HVInfo is
@@ -9,8 +10,9 @@ begin
     -- Try CPUID checks first
     if Hypervisor_Present then
         Put_Line (To_String (Get_Vendor_Name));
+        Set_Exit_Status (0);
     else
-        null;
+        Set_Exit_Status (1);
     end if;
 end HVInfo;
 
