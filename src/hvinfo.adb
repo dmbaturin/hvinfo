a0addb5d8e568493dc659039487eb5b95c6da530
Daniil Baturin <daniil@baturin.org>
2015-03-05 18:11:23 +0600

    Use unbounded IO.

--- src/hvinfo.adb
+++ src/hvinfo.adb
@@ -1,6 +1,4 @@
-with Interfaces; use Interfaces;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
 with Ada.Command_Line; use Ada.Command_Line;
 
 with Hypervisor_Check; use Hypervisor_Check;
@@ -9,7 +7,7 @@ begin
 
     -- Try CPUID checks first
     if Hypervisor_Present then
-        Put_Line (To_String (Get_Vendor_Name));
+        Put_Line (Get_Vendor_Name);
         Set_Exit_Status (0);
     else
         Set_Exit_Status (1);
